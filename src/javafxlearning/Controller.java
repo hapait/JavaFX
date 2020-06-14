614f625dc39175aa033d3a1336ca59f6e7e8b61b
hapait <s.jamil1993@gmail.com>
2020-06-14 12:30:16 +0600

    FXML button click error fixed

--- src/javafxlearning/Controller.java
+++ src/javafxlearning/Controller.java
@@ -1,7 +1,11 @@
 package javafxlearning;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
 public class Controller {
-    private void handleButtonAction(){
+    @FXML
+    private void handleButtonAction(ActionEvent event){
         System.out.println("wow it's working");
     }
 }
