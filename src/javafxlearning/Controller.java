df9d9f0b07147b4d4876675079b37fea9dd5bc23
hapait <s.jamil1993@gmail.com>
2020-06-14 12:45:55 +0600

    initializers and binding done

--- src/javafxlearning/Controller.java
+++ src/javafxlearning/Controller.java
@@ -1,11 +1,19 @@
 package javafxlearning;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 
-public class Controller {
+public class Controller implements Initializable{
     @FXML
     private void handleButtonAction(ActionEvent event){
         System.out.println("wow it's working");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Loading data...");
     }
 }
